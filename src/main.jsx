ea1d0075f7d755ea2cf3110e8abaa552d9c2bef1
afif-gh99 <afif.ghaziri2004@gmail.com>
2026-04-01 11:49:08 +0300

    setup router and install dependencies

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Root from "./components/Root";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [],
+  },
+]);
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
