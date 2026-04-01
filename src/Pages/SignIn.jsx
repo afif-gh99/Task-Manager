7f202f146051665ef1bfed8d1e45968eae13708d
abdullahalhakim-14 <abdullah.alhakim04@gmail.com>
2026-04-01 12:29:11 +0300

    setup routes and pages

---
+++ src/Pages/SignIn.jsx
@@ -0,0 +1,10 @@
+import Sign from "../components/Sign";
+
+const SignIn = () => {
+  return;
+  <div>
+    <Sign />
+  </div>;
+};
+
+export default SignIn;
---
+++ src/components/Sign.jsx
@@ -0,0 +1,11 @@
+const Sign = () => {
+  return;
+  <div>
+    <div>
+      <img src="assets/proteam.logo.png" alt="Proteam logo" />
+      <h1>ProTeam</h1>
+    </div>
+  </div>;
+};
+
+export default Sign;
