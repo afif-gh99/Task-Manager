e802fa24f6d4e2a900f9ec18de6b7a9a31e486cd
abdullahalhakim-14 <abdullah.alhakim04@gmail.com>
2026-04-11 20:07:53 +0300

    add auth system with social buttons

--- src/Pages/SignIn.jsx
+++ src/Pages/SignIn.jsx
@@ -1,10 +1,63 @@
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import Sign from "../components/Sign";
 
-const SignIn = () => {
-  return;
-  <div>
-    <Sign />
-  </div>;
+const Login = () => {
+  const navigate = useNavigate();
+
+  const loginFields = [
+    {
+      name: "email",
+      type: "email",
+      placeholder: "Email Address",
+    },
+    {
+      name: "password",
+      type: "password",
+      placeholder: "Password",
+    },
+  ];
+
+  const handleLogin = async (data) => {
+    console.log("Login Data:", data);
+
+    // try {
+    //   const res = await axios.post("LOGIN_API", data);
+    //   localStorage.setItem("token", res.data.token);
+    //   localStorage.setItem("user", JSON.stringify(res.data.user));
+    //   navigate("/dashboard");
+    // } catch (error) {
+    //   console.log(error);
+    // }
+
+    navigate("/dashboard");
+  };
+
+  const handleGoogleClick = () => {
+    console.log("Google login");
+    // window.location.href = "YOUR_GOOGLE_AUTH_API";
+  };
+
+  const handleFacebookClick = () => {
+    console.log("Facebook login");
+    // window.location.href = "YOUR_FACEBOOK_AUTH_API";
+  };
+
+  return (
+    <Sign
+      title="Welcome back!"
+      subtitle="Sign in to get started with your dashboard"
+      fields={loginFields}
+      buttonText="Sign In"
+      bottomText="Dont have an account?"
+      bottomLinkText="Sign Up"
+      bottomLinkTo="/signup"
+      onSubmit={handleLogin}
+      showForgotPassword={true}
+      onGoogleClick={handleGoogleClick}
+      onFacebookClick={handleFacebookClick}
+    />
+  );
 };
 
-export default SignIn;
+export default Login;
